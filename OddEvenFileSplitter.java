8486564f980c896072a798594c36a525320921c6
usrhome <special_web@mail.ru>
2015-02-24 14:40:58 +0300

    doTaskWithMethodsFromFileSplitter

--- OddEvenFileSplitter.java
+++ OddEvenFileSplitter.java
@@ -5,25 +5,43 @@ public class OddEvenFileSplitter {
 
     public static void main(String[] args) throws IOException {
         int i = 0;
-        File f1 = new File(args[0] + ".txt");
-        File f2 = new File(args[1] + ".txt");
-        File f3 = new File(args[2] + ".txt");
-        f2.createNewFile();
-        f3.createNewFile();
         String s = new String();
-        Reader r1 = new FileReader(f1);
-        Writer w2 = new FileWriter(f2);
-        Writer w3 = new FileWriter(f3);
+        Reader r1 = new FileReader(getSourceFilePath(args[0] + ".txt"));
+        Writer w2 = new FileWriter(getOddLinesFilePath(args[1] + ".txt"));
+        Writer w3 = new FileWriter(getEvenLinesFilePath(args[2] + ".txt"));
         BufferedReader r = new BufferedReader(r1);
         while ((s = r.readLine()) != null) {
             if ((i % 2) == 0)
-            w2.write(s + "\n");
+                w2.write(s + "\n");
             else w3.write(s + "\n");
             i++;
         }
         r1.close();
         w2.close();
         w3.close();
+    }
+    static String getSourceFilePath(String smine){
+        File f1 = new File(smine);
+        return smine;
+    }
+
+    static String getOddLinesFilePath(String s1mine){
+        File f2 = new File(s1mine);
+        try {
+            f2.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return s1mine;
+    }
+    static String getEvenLinesFilePath(String s2mine) {
+        File f3 = new File(s2mine);
+        try {
+            f3.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return s2mine;
     }
 
 }
